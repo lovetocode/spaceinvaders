59e0cf77d16dd84704ce4b978d8c5e64df076b93
lovetocode <dshaggy28@yahoo.com>
2022-05-18 17:33:03 -0700

    enemy movement

--- Enemy/Enemy.gd
+++ Enemy/Enemy.gd
@@ -1,19 +1,36 @@
 extends KinematicBody2D
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var is_waling_to_right =  true
+var start_pos = 0
+var walk_distance = 500
 var speed = 200
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	set_physics_process(true)
-
+	start_pos = 500
+	#set_physics_process(true)
+	
+func walk():
+	if position.x > start_pos + walk_distance:
+		is_waling_to_right = false
+	if position.x < start_pos - walk_distance:
+		is_waling_to_right = true
+		
+	if is_waling_to_right:
+		walk_right()
+	else:
+		walk_left()
+	
+func walk_right():
+	position.x += 5
+	
+func walk_left():
+	position.x -= 5
 
 func _physics_process(delta):
-	move_and_collide(Vector2(speed * delta, 0))
-	if position.x > 1000:
+	#move_and_collide(Vector2(speed * delta, 0))
+	walk()
 		
 	
 		
